26bad3d0974221389e29b8c655da84f219874d29
Sindre10Kodehode <sindre10kodehode@gmail.com>
2023-12-12 13:35:03 +0100

    Portfolio D2

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// @type {import('tailwindcss').config}
+// module.exports = {
+//   theme: {
+//     extend: {
+//       colors: {
+//         Mauve: "#e2c4f6",
+//         darkPurple: "#420039",
+//         charcoal: "#28536b",
+//       },
+//     },
+//   },
+// };
